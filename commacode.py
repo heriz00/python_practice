965f3cae38c952080c551e1a704787d254a459f5
Heriz Bista <herizbista@gmail.com>
2025-02-21 23:48:59 +0545

    changed the logic so that it handles two list only

--- commacode.py
+++ commacode.py
@@ -1,10 +1,29 @@
 def commaSep(newList):
+    
     newString = ""
+    
     for index, item in enumerate(newList):
-        if index < len(newList) - 1:
-            newString = newString + str(item) + ", " 
-        else:
-            newString = newString + "& " + str(item)
+        
+        if len(newList) > 1 and len(newList) != 2:
+            
+            if index < len(newList) - 1:
+                newString = newString + str(item) + ", " 
+
+            else:
+                newString = newString + "& " + str(item)
+         
+        elif len(newList) == 2:
+            
+            if index < len(newList) - 1:
+                newString =  newString + str(item) + " & " 
+
+            else:
+                newString = newString + str(item)
+            
+           
+        else: 
+            newString = str(item)
+            
     return newString
     
 spam = input("Give me any list: ")
