932d1b4ab9b2a8cc294617a23ec4bb71c24391db
Heriz Bista <herizbista@gmail.com>
2025-02-22 00:59:01 +0545

    reduced the code significantly

--- coinflipstreaks.py
+++ coinflipstreaks.py
@@ -3,35 +3,35 @@ import random
 coin = []
 
 i = 1
-counter = 0
+h_flip = 0
+t_flip = 0
 h_streak = 0
 t_streak = 0
 
-while i <= 100000:
+while i <= 10000:
     flip = random.choice(["H","T"])
     coin.append(flip)
     i += 1
 
-for index,item in enumerate(coin):
+
+for flip in coin:
     
-    last_item = []
-    
-    if item == 'H':
-        last_item.append('H')
+    if flip == 'H':
+        h_flip += 1
+        t_flip = 0
         
-        if 'T' not in last_item and counter == 6:
-            h_streak = h_streak + 1
-            counter = 0
+        if h_flip == 6:
+            h_streak += 1
+            h_flip = 0
                 
     else:
-        last_item.append("T")
-    
-        if 'H' not in last_item and len(last_item) == counter:
-            t_streak = t_streak + 1
-            counter = 0
-            
-    counter += 1
-    
+        t_flip +=1
+        h_flip = 0
+        
+        if t_flip ==6:
+            t_streak += 1
+            t_flip = 0
+               
 print(f"The streak of head is {h_streak}")
 print(f"The streak of tail is {t_streak}")
     
