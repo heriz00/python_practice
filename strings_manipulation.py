2293caec3411f018a479ba8eb93dc12ded34e7c3
Heriz Bista <herizbista@gmail.com>
2025-03-10 20:51:05 +0545

    will commit multiple times in this file

---
+++ strings_manipulation.py
@@ -0,0 +1,22 @@
+# print("Hello There! \nHow are you? \nI\'m doing fine.")
+# print(r"Hi Heriz\'s Mom")
+# print('''Dear Mom,
+      
+# I Hope you're doing fine there.
+
+# I am alright here.''')
+
+# spam = "Hello, World!"
+# times = 5
+# # print(spam[:-1])
+# # print("Hello" in spam)
+# print ("I am printing %s %s times." %(spam, times))
+# print (f"I'm printing {spam} {times} times.")
+
+print ("How are you feeling?")
+
+mood = input()
+if mood.lower() == "great":
+    print("I feel great too.")
+else:
+    print("It will pass.")
