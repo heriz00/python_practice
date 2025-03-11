39881aa9380547876120db05c17fba95a1fe6de0
Heriz Bista <herizbista@gmail.com>
2025-03-11 21:49:34 +0545

    used pyperclip library to copy, and also learned about command line argument

---
+++ mclip.py
@@ -0,0 +1,22 @@
+#mclip.py-A multiclipboard program
+
+TEXT = {'agree': '''Yes, I agree. That sounds fine to me''', 
+        'busy': '''Sorry, can we do this later this week or next week? I am super busy. Thank you.''',
+        'upsell': '''Would you consider making this a monthly donation?'''}
+
+
+import sys, pyperclip 
+
+if len(sys.argv) < 2:
+        print ("Usage: python mclip.py [keyphrase] - copy phrase text")
+        sys.exit()
+
+keyphrase = sys.argv[1]
+
+if keyphrase in TEXT:
+        pyperclip.copy(TEXT[keyphrase])
+        print("Text for " + keyphrase + " copied to clipboard.")
+
+else:
+        print("There is no text for " + keyphrase + ".")
+
